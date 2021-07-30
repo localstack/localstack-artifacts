546248735564df4fc06f6fe2b1d4a5930ca97c56
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-07-30 10:18:02 +0200

    add scripts and assets for patched DynamoDB Local jar

---
+++ dynamodb-local-patch/README.md
@@ -0,0 +1,3 @@
+# DynamoDBLocal Assets
+
+Pre-built assets for DynamoDBLocal, for use in Alpine Docker containers.
---
+++ dynamodb-local-patch/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+test -e dynamodb_local_latest.zip || \
+  wget https://s3.us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.zip
+
+# prepare contents
+rm -rf build/
+mkdir -p build
+cp dynamodb_local_latest.zip build/
+(
+  cd build
+  unzip dynamodb_local_latest.zip
+  rm dynamodb_local_latest.zip
+  # remove unused libs
+  rm -f DynamoDBLocal_lib/libsqlite4java-linux-aarch64.so
+  rm -f DynamoDBLocal_lib/libsqlite4java-linux-i386.so
+  rm -f DynamoDBLocal_lib/libsqlite4java-osx.dylib
+  rm -f DynamoDBLocal_lib/sqlite4java-win32-x64.dll
+  rm -f DynamoDBLocal_lib/sqlite4java-win32-x86.dll
+)
+
+cp etc/libsqlite4java-linux-amd64.so build/DynamoDBLocal_lib/
+
+# create zip file
+(cd build; zip -r DynamoDBLocal.zip *)
+cp build/DynamoDBLocal.zip etc/
