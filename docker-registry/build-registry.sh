b60f6a744627d2d83e377a0845c38f7086106ac1
Daniel Fangl <daniel.fangl@localstack.cloud>
2021-12-20 21:40:31 +0100

    add registry builds for supported architectures (linux and macos, arm64/amd64) (#3)

---
+++ docker-registry/build-registry.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+(cd distribution && make bin/registry)
+registry_file_name=registry-$GOOS-$GOARCH
+cp distribution/bin/registry ${registry_file_name}
+zip ${registry_file_name}.zip ${registry_file_name}
+rm ${registry_file_name}
+(cd distribution && make clean)
