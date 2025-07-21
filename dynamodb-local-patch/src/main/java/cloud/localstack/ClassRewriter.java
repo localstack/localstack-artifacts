fb61d469f13b480c581e033f293db5591f784401
Giovanni Grano <me@giograno.com>
2025-07-21 13:46:21 +0200

    new patch

--- dynamodb-local-patch/src/main/java/cloud/localstack/ClassRewriter.java
+++ dynamodb-local-patch/src/main/java/cloud/localstack/ClassRewriter.java
@@ -12,7 +12,10 @@ import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
 public class ClassRewriter {
-    static String CLASS_NAME = "com.amazonaws.services.dynamodbv2.local.shared.access.api.cp.CreateTableFunction";
+    static String[] CLASS_NAMES = {
+            "com.amazonaws.services.dynamodbv2.local.shared.access.api.cp.CreateTableFunction",
+            "software.amazon.dynamodb.services.local.shared.access.api.cp.CreateTableFunction"
+    };
 
     public static void premain(String agentArgs, Instrumentation inst) {
         inst.addTransformer(new ClassFileTransformer() {
@@ -21,26 +24,28 @@ public class ClassRewriter {
                     throws IllegalClassFormatException {
 
                 if (s.endsWith("api/cp/CreateTableFunction")) {
-                    try {
-                        ClassPool cp = ClassPool.getDefault();
-                        CtClass cc = cp.get(CLASS_NAME);
-                        CtMethod m = cc.getDeclaredMethod("apply");
+                    for (String class_name : CLASS_NAMES) {
+                        try {
+                            ClassPool cp = ClassPool.getDefault();
+                            CtClass cc = cp.get(class_name);
+                            CtMethod m = cc.getDeclaredMethod("apply");
 
-                        CodeIterator codeIterator = m.getMethodInfo().getCodeAttribute().iterator();
-                        while (codeIterator.hasNext()) {
-                            int pos = codeIterator.next();
-                            int opcode = codeIterator.byteAt(pos);
+                            CodeIterator codeIterator = m.getMethodInfo().getCodeAttribute().iterator();
+                            while (codeIterator.hasNext()) {
+                                int pos = codeIterator.next();
+                                int opcode = codeIterator.byteAt(pos);
 
-                            if (opcode == Opcode.BIPUSH && codeIterator.byteAt(pos + 1) == 20) {
-                                // Change "if(gsiIndexes.size() > 20)" to "if(gsiIndexes.size() > 100)"
-                                codeIterator.writeByte(100, pos + 1);
+                                if (opcode == Opcode.BIPUSH && codeIterator.byteAt(pos + 1) == 20) {
+                                    // Change "if(gsiIndexes.size() > 20)" to "if(gsiIndexes.size() > 100)"
+                                    codeIterator.writeByte(100, pos + 1);
+                                }
                             }
+                            byte[] byteCode = cc.toBytecode();
+                            cc.detach();
+                            return byteCode;
+                        } catch (Throwable ex) {
+                            ex.printStackTrace();
                         }
-                        byte[] byteCode = cc.toBytecode();
-                        cc.detach();
-                        return byteCode;
-                    } catch (Throwable ex) {
-                        ex.printStackTrace();
                     }
                 }
                 return null;
